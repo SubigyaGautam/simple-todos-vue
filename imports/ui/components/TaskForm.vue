93b99a5cbba2db4b8fdc2fa1dd98c8db5371561f
Subigya Gautam <subigya.gautam@keela.com>
2022-10-15 10:12:49 +0545

    Added components for the to-do list

---
+++ imports/ui/components/TaskForm.vue
@@ -0,0 +1,35 @@
+<template>
+  <form class="task-form" @submit.prevent="handleSubmit">
+    <input
+        type="text"
+        placeholder="Type to add new tasks"
+        v-model="newTask"
+    />
+    <button type="submit">Add Task</button>
+  </form>
+</template>
+
+<script>
+import Vue from "vue";
+import { Meteor } from 'meteor/meteor';
+import { TasksCollection } from "../../db/TasksCollection";
+
+export default {
+  data() {
+    return {
+      newTask: ""
+    }
+  },
+  methods: {
+    handleSubmit(event) {
+      alert(this.newTask);
+      if (this.newTask.length === 0) return;
+
+      Meteor.call('tasks.insert', this.newTask);
+
+      // Clear form
+      this.newTask = "";
+    }
+  },
+}
+</script>
---
+++ server/main.js
@@ -0,0 +1,38 @@
+import { Meteor } from 'meteor/meteor';
+import { Accounts } from 'meteor/accounts-base';
+import { TasksCollection } from '/imports/db/TasksCollection';
+import '/imports/api/tasksMethods';
+import '/imports/api/tasksPublications';
+
+const insertTask = (taskText, user) =>
+  TasksCollection.insert({
+    text: taskText,
+    userId: user._id,
+    createdAt: new Date(),
+  });
+
+const SEED_USERNAME = 'met';
+const SEED_PASSWORD = 'password';
+
+Meteor.startup(() => {
+  if (!Accounts.findUserByUsername(SEED_USERNAME)) {
+    Accounts.createUser({
+      username: SEED_USERNAME,
+      password: SEED_PASSWORD,
+    });
+  }
+
+  const user = Accounts.findUserByUsername(SEED_USERNAME);
+
+  if (TasksCollection.find().count() === 0) {
+    [
+      'First Task from server code',
+      'Second Task from server code',
+      'Third Task from server code',
+      'Fourth Task from server code',
+      'Fifth Task from server code',
+      'Sixth Task from server code',
+      'Seventh Task from server code',
+    ].forEach(taskText => insertTask(taskText, user));
+  }
+});
